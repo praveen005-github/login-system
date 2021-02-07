16a0089bc982f5a6408c7a5bce53b98d954100b1
Praveen <Attractivepraveen25@gmail.com>
2021-02-08 00:38:55 +0530

    Incomplete Protected.js

--- login_app/src/cmp/Protected.js
+++ login_app/src/cmp/Protected.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-
-function Protected()
+import{ Redirect } from 'react-router-dom';
+function Protected(props)
 {
+    const Cmp = props.cmp
+    var auth = JSON.parse(localStorage.getItem('auth'))
+    console.warn(auth)
+
     return <div> Protected </div>
+
 }
+
 
 
 export default Protected;
